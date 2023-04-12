3370cec5dd494f996cc12235a02f4929dfbf1fe5
Diego Ferreira <diegomecanica2014@gmail.com>
2023-04-11 21:55:28 -0400

    configurando a pagina de interacao com o cliente

---
+++ includes/head.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Plataforma de anuncio de veiculos</title>
+    <link rel="stylesheet" type="text/css" href="views/style.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <div class="main">
+        
+
+
+
+
+</head>
---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+
+<?php include 'includes/head.php'; ?>
+
+
+<body>
+  <center>
+<figure class="figure">
+  <img src="img/chatgpt400px.jpg" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
+  <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
+</figure></center>
+<form>
+<center>
+  <center><div class="form-group">
+    <label for="exampleFormControlTextarea1">Digite sua pergunta</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+  </div></center>
+</form>
+</center>
+    
+</body>
+</html>
