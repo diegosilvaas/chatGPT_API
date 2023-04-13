314675cba00d90869a69006a0ea79c7b95a9988b
Diego Ferreira <diegomecanica2014@gmail.com>
2023-04-12 22:29:35 -0400

    adicionando o button enviar no formulario

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="PT-BR">
 
@@ -8,18 +7,26 @@
 
 <body>
   <center>
-<figure class="figure">
-  <img src="img/chatgpt400px.jpg" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-  <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
-</figure></center>
-<form>
-<center>
-  <center><div class="form-group">
-    <label for="exampleFormControlTextarea1">Digite sua pergunta</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-  </div></center>
-</form>
-</center>
-    
+    <figure class="figure">
+      <img src="img/chatgpt400px.jpg" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
+      <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
+    </figure>
+  </center>
+  <form>
+    <center>
+      <center>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Digite sua pergunta</label>
+          <textarea class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="5"></textarea>
+        </div>
+      </center>
+  </form>
+  
+  <div>
+    <button type="submit" class="btn btn-dark">Enviar</button>
+  </div>
+  
+
 </body>
+
 </html>
