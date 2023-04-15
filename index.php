462c9686130772dc4e4d7cf25b529d68c9cf58b9
Diego Ferreira <diegomecanica2014@gmail.com>
2023-04-15 16:53:45 -0400

    implementando melhorias na interface

--- index.php
+++ index.php
@@ -8,23 +8,24 @@ session_start();
 
 
 <body>
+<div class="container">
   <center>
-    <figure class="figure">
+   <br> <figure class="w-50 p-3" class="figure">
       <img src="img/chatgpt400px.jpg" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-      <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
+      <figcaption class="figure-caption"></figcaption>
     </figure>
   </center>
 
 
   <center>
    
-
+ 
       <form action="action.php" method="post" id="form">
-        <div class="form-group">
+        <div class="w-100 p-3"  class="form-group">
 
-          <label for="exampleFormControlTextarea1">Digite sua pergunta</label>
+          <h2><label for="exampleFormControlTextarea1">Digite sua pergunta</label></h2>
           <textarea class="form-control form-control-lg" name="question" id="exampleFormControlTextarea1" rows="5"></textarea>
-          <br><button type="submit" name="btncadastrar" class="btn btn-dark">Enviar</button>
+          <br><button type="submit" name="btncadastrar" class="btn btn-primary">Enviar</button>
         </div>
         </form>
   </center>
@@ -42,7 +43,7 @@ session_start();
       <p class="mb-0"></p>
     </div>
   </center>
-
+ </div>
 
   <script>
 
